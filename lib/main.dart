d68235dc08f65ddf31d347339df65e4531863403
Hassan Khalid <m.hassankhalid39@gmail.com>
2024-10-02 19:17:07 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Shimmer Plugin',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
           useMaterial3: true,
         ),
         home: const HomeScreen());
